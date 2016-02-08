b91b36db3c89c14a7448eb74ced59afb94a7b46f
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2016-02-09 00:28:35 +0100

    Add solution 1

---
+++ day02.py
@@ -0,0 +1,26 @@
+def get_lwh(line):
+    list = line.split('x')
+    return int(list[0]), int(list[1]), int(list[2])
+
+
+def get_cuboid_area(l, w, h):
+    return (2 * l * w + 2 * l * h + 2 * w * h)
+
+
+def get_smallest_face_area(l, w, h):
+    return min(l * w, l * h, w * h)
+
+
+def day2_1():
+    area = 0
+
+    with open('data/02') as data:
+        for lines in data:
+            l, w, h = get_lwh(lines)
+            area += get_cuboid_area(l, w, h)
+            area += get_smallest_face_area(l, w, h)
+
+    print('area =', area)
+
+
+day2_1()
