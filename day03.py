9702dca36af3a13033240ab15eeba080628021e7
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2016-02-09 01:03:22 +0100

    Add solution 2

--- day03.py
+++ day03.py
@@ -23,4 +23,29 @@ def day3_1():
     print('houses =', len(houses))
 
 
+def day3_2():
+    santa_pos = 0 + 0j
+    robo_pos = 0 + 0j
+    houses = set()
+    houses.add(santa_pos)
+    houses.add(robo_pos)
+
+    santa_turn = True
+
+    with open('data/03') as data:
+        for lines in data:
+            for ch in lines:
+                if santa_turn:
+                    santa_pos += get_shift_from_instruction(ch)
+                    houses.add(santa_pos)
+                    santa_turn = False
+                else:
+                    robo_pos += get_shift_from_instruction(ch)
+                    houses.add(robo_pos)
+                    santa_turn = True
+
+    print('houses =', len(houses))
+
+
 day3_1()
+day3_2()
