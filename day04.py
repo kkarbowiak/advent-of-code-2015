13f4013b01dcd477af8be7a866795d464775e1a0
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2016-02-09 01:37:52 +0100

    Add solution 2

--- day04.py
+++ day04.py
@@ -22,4 +22,18 @@ def day4_1():
 
         print('noonce =', noonce)
 
+
+def day4_2():
+    with open('data/04') as data:
+        secret = data.readline()
+        noonce = 0
+        hex = get_digest(secret, noonce)
+        while not hex.startswith('000000'):
+            noonce += 1
+            hex = get_digest(secret, noonce)
+
+        print('noonce =', noonce)
+
+
 day4_1()
+day4_2()
