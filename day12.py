fdd17db58b999cea45da645bcb7ef3a57185da6c
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2016-02-16 22:28:40 +0100

    Add solution 2

--- day12.py
+++ day12.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 
 def get_sum(json):
@@ -10,6 +11,31 @@ def get_sum(json):
     return sum
 
 
+def should_remove(object):
+    return type(object) == type({}) and 'red' in object.values()
+
+
+def remove_red_objects(object):
+    if type(object) == type([]):
+        i = 0
+        size = len(object)
+        while i < size:
+            remove_red_objects(object[i])
+
+            if should_remove(object[i]):
+                del object[i]
+                size -= 1
+            else:
+                i += 1
+            
+    elif type(object) == type({}):
+        for k, v in list(object.items()):
+            remove_red_objects(v)
+
+            if should_remove(v):
+                del object[k]
+
+
 def day12_1():
     with open('data/12') as data:
         json = data.readline()
@@ -19,4 +45,16 @@ def day12_1():
         print('sum =', sum)
 
 
+def day12_2():
+    with open('data/12') as data:
+        object = json.load(data)
+        remove_red_objects(object)
+
+        dump = json.dumps(object)
+        sum = get_sum(dump)
+
+        print('sum =', sum)
+
+
 day12_1()
+day12_2()
