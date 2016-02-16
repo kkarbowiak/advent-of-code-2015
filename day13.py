552e23e2be35b586ee405a7b9a80763f20938015
kkarbowiak <kkarbowiak@localhost>
2016-02-16 23:01:38 +0100

    Add solution 2

--- day13.py
+++ day13.py
@@ -11,10 +11,23 @@ def get_happiness_from_instruction(line):
         return m.group(1), m.group(4), -int(m.group(3))
 
 
-def day13_1():
+def get_max_happiness(people, happinesses):
+    max_happiness = -999999999999
+
+    for order in itertools.permutations(people):
+        score = 0
+        for index in range(len(order)):
+            next = (index + 1) % len(order)
+            score += happinesses[order[index], order[next]]
+            score += happinesses[order[next], order[index]]
+        max_happiness = max(max_happiness, score)
+
+    return max_happiness
+
+
+def get_relations(data):
     happinesses = {}
     people = set()
-    max_happiness = -999999999999
     with open('data/13') as data:
         for line in data:
             n1, n2, happiness = get_happiness_from_instruction(line)
@@ -22,15 +35,36 @@ def day13_1():
             people.add(n1)
             people.add(n2)
 
-        for order in itertools.permutations(people):
-            score = 0
-            for index in range(len(order)):
-                next = (index + 1) % len(order)
-                score += happinesses[order[index], order[next]]
-                score += happinesses[order[next], order[index]]
-            max_happiness = max(max_happiness, score)
+    return people, happinesses
+
+
+def day13_1():
+    happinesses = {}
+    people = set()
+    with open('data/13') as data:
+        people, happinesses = get_relations(data)
+
+    max_happiness = get_max_happiness(people, happinesses)
+
+    print('happiness =', max_happiness)
+
+
+def day13_2():
+    happinesses = {}
+    people = set()
+    with open('data/13') as data:
+        people, happinesses = get_relations(data)
+
+    for person in people:
+        happinesses[person, 'me'] = 0
+        happinesses['me', person] = 0
+
+    people.add('me')
+
+    max_happiness = get_max_happiness(people, happinesses)
 
     print('happiness =', max_happiness)
 
 
 day13_1()
+day13_2()
