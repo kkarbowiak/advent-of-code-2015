3ad3d1a8269f6b2bafdf4e804d427a630b7be2db
kkarbowiak <kkarbowiak@localhost>
2016-02-23 16:09:08 +0100

    Add solution 2

--- day20.py
+++ day20.py
@@ -20,4 +20,29 @@ def day20_1():
     print('house =', house)
 
 
+def day20_2():
+    presents = None
+    with open('data/20') as data:
+        presents = int(data.readline())
+
+    house = 0
+    l = [0] * presents
+
+    for i in range(1, presents + 1):
+        j = i
+        d = 0
+        while j < presents and d < 50:
+            l[j] += 11 * i
+            j += i
+            d += 1
+
+    for i in range(1, presents + 1):
+        if l[i] >= presents:
+            house = i
+            break
+
+    print('house =', house)
+
+
 day20_1()
+day20_2()
