a44d8ce51a7ece0bd5804d1f24aa6033a4f2495e
kkarbowiak <kkarbowiak@localhost>
2016-03-05 00:26:46 +0100

    Add solution 2

--- day24.py
+++ day24.py
@@ -34,4 +34,27 @@ def day24_1():
     print('min_qe =', min_qe)
 
 
+def day24_2():
+    items = []
+    with open('data/24') as data:
+        for line in data:
+            items.append(int(line))
+
+    min_qe = 999999999999
+
+    found =  False
+
+    for g1len in range(1, len(items) - 1):
+        for g1 in itertools.combinations(items, g1len):
+            g2g3 = [i for i in items if i not in g1]
+            if get_weight(g2g3) == 3 * get_weight(g1):
+                min_qe = min(min_qe, get_qe(g1))
+                found = True
+        if found:
+            break
+
+    print('min_qe =', min_qe)
+
+
 day24_1()
+day24_2()
